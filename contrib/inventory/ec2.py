05a0024fb10b1ef0b8439f0d0d91fd1400fc3123
Marc Tamsky <tamsky@users.noreply.github.com>
2015-11-04 15:32:47 -0800

    w.i.p. ec2.py gets multithreaded boto, not working yet.

--- contrib/inventory/ec2.py
+++ contrib/inventory/ec2.py
@@ -140,6 +140,78 @@ except ImportError:
     import simplejson as json
 
 
+DEFAULT_NUMBER_OF_WORKERS = 3
+
+class BotoWorkerPool:
+
+    def __init__(self, workers=None, start_with=None):
+        if workers is None:
+            self.workers = len(self.regions()) + 1
+        else:
+            self.workers = DEFAULT_NUMBER_OF_WORKERS
+        self.locks = [ ]
+        self.threads = [ ]
+        self.queue = Queue(0)
+        self.resqueue = Queue(0)    # results queue
+
+        if start_with is not None:
+            self.enqueue(eval("self.%s" % start_with))
+
+    def enqueue(self, *args):
+        if len(args) == 1:
+            self.queue.put(args[0])
+        else:
+            self.queue.put(args)
+
+    def run(self):
+        assert not self.queue.empty(), "run() called with empty queue"
+
+        for i in range(self.workers):
+            self.locks.append(Lock())
+            self.threads.append(Thread(target=self.__worker, name=str(i),
+                args=(self.locks[-1],)))
+            self.threads[-1].start()
+
+        while True:
+            has_busy_thread = False
+            for lock in self.locks:
+                if lock.acquire(False):
+                    lock.release()
+                else:
+                    has_busy_thread = True
+                    break
+            if not has_busy_thread and self.queue.empty():
+                [ self.enqueue("exit") for i in range(self.workers) ]
+                break
+            else:
+                time.sleep(0.5)
+
+        for thread in self.threads: thread.join()
+
+        self.results = [ ]
+        while True:
+            try: self.results.append(self.resqueue.get_nowait())
+            except Empty: break
+        return self.results
+
+    def __worker(self, lock):
+        while True:
+            task = self.queue.get()
+            if type(task) is str and task == "exit": break
+            if callable(task):
+                _callable, _args = task, ()
+            else:
+                _callable, _args = task[0], task[1:]
+
+            lock.acquire()
+            try:
+                apply(_callable, _args)
+            except Exception, e:
+                # FIXME
+                print e
+            lock.release()
+
+
 class Ec2Inventory(object):
     def _empty_inventory(self):
         return {"_meta" : {"hostvars" : {}}}
@@ -409,10 +481,19 @@ class Ec2Inventory(object):
     def do_api_calls_update_cache(self):
         ''' Do API calls to each region, and save data in cache files '''
 
+        sys.stderr.write("Fetching...")
+        sys.stderr.flush()
         if self.route53_enabled:
+            sys.stderr.write("Route53...")
+            sys.stderr.flush()
             self.get_route53_records()
+            sys.stderr.write("done.  ")
+            sys.stderr.flush()
 
+        sys.stderr.write("ec2 instances in each region...")
+        sys.stderr.flush()
         for region in self.regions:
+            sys.stderr.write("{},".format(region))
             self.get_instances_by_region(region)
             if self.rds_enabled:
                 self.get_rds_instances_by_region(region)
@@ -1309,5 +1390,6 @@ class Ec2Inventory(object):
 
 
 # Run the script
+pool = BotoWorkerPool(workers=3, start_with="get_instances_by_region")
 Ec2Inventory()
 
